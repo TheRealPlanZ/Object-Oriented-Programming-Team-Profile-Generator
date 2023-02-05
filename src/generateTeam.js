2ed6693fecf6935b9a0b989b07e19ab2df4edd0e
Zack Levin <112544104+TheRealPlanZ@users.noreply.github.com>
2023-02-04 20:56:12 -0700

    Moved files to an output folder

--- src/generateTeam.js
+++ src/generateTeam.js
@@ -5,7 +5,7 @@ const generateTeam = team => {
         <div class="card employee-card">
             <div class="card-header">
             <h2 class="card-title">${manager.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>${manager.getRole()}</h3>
+            <h3 class="card-title">${manager.getRole()}</h3>
             </div>
             <div class="card-body">
             <ul class="list-group">
@@ -22,7 +22,7 @@ const generateTeam = team => {
         <div class="card employee-card">
             <div class="card-header">
             <h2 class="card-title">${engineer.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>${engineer.getRole()}</h3>
+            <h3 class="card-title">>${engineer.getRole()}</h3>
             </div>
             <div class="card-body">
             <ul class="list-group">
@@ -41,7 +41,7 @@ const generateTeam = team => {
         <div class="card employee-card">
             <div class="card-header">
             <h2 class="card-title">${intern.getName()}</h2>
-            <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>${intern.getRole()}</h3>
+            <h3 class="card-title">${intern.getRole()}</h3>
             </div>
             <div class="card-body">
             <ul class="list-group">
@@ -86,14 +86,14 @@ module.exports = team => {
     <body>
         <div class="container-fluid">
             <div class="row">
-                <div class="col-12 jumbotron mb-3 team-heading">
+                <div class="col-12 jumbotron mb-3">
                     <h1 class="text-center">My Team</h1>
                 </div>
             </div>
         </div>
         <div class="container">
             <div class="row">
-                <div class="team-area col-12 d-flex justify-content-center">
+                <div class="col-12 d-flex justify-content-center">
                 ${generateTeam(team)}
                 </div>
             </div>
